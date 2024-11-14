93006a210c5c240b1d3848c0954731e5c2c22027
Granth Agarwal <heygranth@gmail.com>
2024-11-14 16:23:10 +0530

    setup routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint, render_template
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+main = Blueprint('main', __name__)
 
 
-if __name__ == '__main__':
-    app.run()
+@main.route('/')
+def index():  # put application's code here
+    projects = [
+        {'name': "Project 1", 'description': '.....', 'docs_url': "/docs/projects/project1"}
+    ]
+    return render_template('index.html', projects=projects)
+
+
+@main.route('/projects/<project_name>')
+def project(project_name):
+    project_details = []
+    return render_template('project.html', project=project_details)
