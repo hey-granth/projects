e8acef5b4eee53b1fbd300872b7129aa20cb2fd7
Granth Agarwal <heygranth@gmail.com>
2024-11-15 10:43:47 +0530

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = '2023'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +29,10 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown'
+}
